3898dcf1d86c34962ee19d7215a3385ec4e32735
sherg <sgamble284@gmail.com>
2020-03-22 07:56:48 -0400

    added example data to database using seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,3 @@ sheriefs = User.create(name: "Sheriefs", email: "sheriefsboyflash@gmail.com", pa
 
 Reward.create(reward_name: "Milky Way Bar", points_needed: 25, current_points: 5, user_id: khalil.id)
 Reward.create(reward_name: "Play Date with Arlo", points_needed: 50, current_points: 10, user_id: khalil.id)
-
-khalil.rewards.create(reward_name: "Watch youtube video")
